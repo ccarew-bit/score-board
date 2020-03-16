cb3f0d62aaf64d15203d4305a3d65702e490fe32
corinne carew <ccarew@mail.usf.edu>
2020-03-16 16:01:20 -0400

    almost done maybe

--- public/main.js
+++ public/main.js
@@ -1,7 +1,35 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  console.log('hello,world')
 }
 
+const calculateaddScore = addScoreAmount => {
+  console.log(addScoreAmount + 'button clicked')
+  const total = 'team-1-score' + 1
+  const scoreTotal = parseFloat(total)
+  console.log(scoreTotal)
+}
+
+const calculateMinusScore = minusScoreAmount => {
+  console.log(addScoreAmount + 'button clicked')
+  const total = 'team-1-score' - 1
+  const scoreTotal = parseFloat(total)
+  console.log(scoreTotal)
+}
+
+// const addOne = () => {
+//   calculateaddScore()
+// }
+
+// const minusOne = () => {
+//   calculateMinusScore()
+// }
+
 document.addEventListener('DOMContentLoaded', main)
+
+document
+  .querySelector('.team-1-add-1-button')
+  .addEventListener('click', calculateaddScore)
+
+document
+  .querySelector('team-1-subtract-1-button')
+  .addEventListener('click', calculateMinusScore)
